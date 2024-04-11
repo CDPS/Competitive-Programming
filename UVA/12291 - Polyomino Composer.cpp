9a0aac5ddde3033be07da372709fcd6a70a1ad0e
Cristian_Palechor <Cristian_Palechor@EPCOBOGW0356>
2024-04-11 18:43:21 -0500

    uploading exercie of hell

---
+++ UVA/12291 - Polyomino Composer.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool v[12][12];
+string a[12], b[12];
+
+int n,m;
+
+struct node{
+    int x,y;
+    node(int x, int y) : x(x), y(y){}
+};
+
+bool isValid(int x, int y){
+    return x >=0 && x < n && y>=0 && y < a[x].size() && a[x][y] == '*' && v[x][y]==false;
+}
+
+bool check(){
+    for(int i=0;i<n;i++)
+        for(int j=0;j<a[i].size();j++)
+            if(a[i][j]=='*' && !v[i][j])
+                return false;
+    return true;
+}
+
+vector<node> getPath(int x,int y){
+    vector<node> path;
+    for(int i=0;i<m;i++)
+        for(int j=0;j<b[i].size();j++)
+            if(b[i][j]=='*')
+                path.push_back(node(i - x, j -y));
+    return path;
+}
+
+bool fit(int x, int y, vector<node> &path){
+    for(int i=0;i<path.size();i++){
+        if(!isValid(x + path[i].x, y + path[i].y))
+            return false;
+    }
+    return true;
+}
+
+void visitPath(int x, int y, vector<node> &path){
+    for(int i=0;i<path.size();i++)
+        v[x + path[i].x][y + path[i].y] = true;
+}
+
+bool solve(vector<node> &path){
+    for(int i=0;i<n;i++)
+        for(int j=0;j<a[i].size();j++)
+            if(a[i][j]=='*' && !v[i][j])
+               if(fit(i,j,path))
+                    visitPath(i, j,path);
+                else
+                    return false;
+
+    return check();
+}
+
+int main(){
+
+    cin.sync_with_stdio(false); cin.tie(NULL);
+
+    while(cin >> n >> m && (n||m) ){
+
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+            for(int j=0;j<a[i].size();j++)
+                 v[i][j]=false;
+        }
+
+        int x = -1, y=-1;
+        bool first = true;
+        for(int i=0;i<m;i++){
+            cin >> b[i];
+            for(int j=0;j<b[i].size();j++){
+                if(b[i][j]=='*' && first)
+                    x = i, y = j, first= false;
+            }
+        }
+
+        vector<node> path = getPath(x,y);
+        cout  << solve(path) << "\n";
+    }
+    return 0;
+}
